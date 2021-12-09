80990e9267de09d14f03dae82619b905067cb689
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-12-09 16:21:47 +0530

    disable accessing plugin command without any subcommands
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- cmd/plugin.go
+++ cmd/plugin.go
@@ -16,36 +16,15 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // pluginCmd represents the plugin command
 var pluginCmd = &cobra.Command{
 	Use:   "plugin",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("plugin called")
-	},
+	Short: "Access subcommands associated with \"plugin\"",
 }
 
 func init() {
 	rootCmd.AddCommand(pluginCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// pluginCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// pluginCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
