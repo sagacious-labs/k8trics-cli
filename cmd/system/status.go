00a952ab75cb0ce76443ce62ae65b810b9ca496b
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-12-13 03:30:46 +0530

    cleanup "status" subcommand
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- cmd/system/status.go
+++ cmd/system/status.go
@@ -26,13 +26,7 @@ import (
 // statusCmd represents the status command
 var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "status shows the status of the K8trics components",
 	Run: func(cmd *cobra.Command, args []string) {
 		if checkHyperion() {
 			color.Green("✅ Hyperion Daemons are running and are in Healthy state")
@@ -50,16 +44,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	SystemCmd.AddCommand(statusCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func checkHyperion() bool {
